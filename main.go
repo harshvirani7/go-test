affe65199ae6b3ddd77d862f1e68b38fe51663df
harshvirani7 <harsh.v@een.com>
2024-05-31 15:38:20 +0530

    updated removePathParam and tested it with a middleware

--- main.go
+++ main.go
@@ -10,12 +10,18 @@ import (
 func main() {
 	r := gin.Default()
 
-	// sample VideoSearch APIs test
-	// Route to capture the groupId with a manual split
-	r.GET("/api/v3/videosearch/events/groups/:groupId/aggregates", func(c *gin.Context) {
-		newPath := removePathParam(c)
-		// newPath := removePathParamOriginal(c)
-		c.String(200, fmt.Sprintf("Original path: %s\nNew path: %s", c.Request.URL.Path, newPath))
+	// Ensure the raw URL path is captured
+	r.UseRawPath = true
+
+	// Add the middleware
+	r.Use(removePathParamMiddleware())
+
+	r.GET("/api/v3/videosearch/events/groups/:groupId/aggregates/value/:valueId", func(c *gin.Context) {
+		c.String(200, fmt.Sprintf("Original path: %s\nNew path: %s", c.Request.URL.Path, c.GetString("newPath")))
+	})
+
+	r.GET("/api/v3/videosearch/events/groups/:groupId/aggregates/var1/:var1Id/var2/:var2Id", func(c *gin.Context) {
+		c.String(200, fmt.Sprintf("Original path: %s\nNew path: %s", c.Request.URL.Path, c.GetString("newPath")))
 	})
 
 	// Start the server
@@ -25,18 +31,24 @@ func main() {
 // Original function from VideoSearch
 func removePathParam(c *gin.Context) string {
 	var newPath string
-	for _, str := range strings.Split(c.Request.URL.Path, "/") {
-		found := false
-		for _, paramValue := range c.Params {
-			if str == paramValue.Value {
-				newPath = newPath + "*/"
-				found = true
-				break
-			}
-		}
-		if !found {
-			newPath = newPath + str + "/"
-		}
+	newPath = c.Request.URL.Path
+	for _, paramValue := range c.Params {
+		newPath = strings.Replace(newPath, paramValue.Value, "*", -1)
 	}
 	return newPath
 }
+
+// Middleware to use removePathParamOriginal
+func removePathParamMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		newPath := removePathParam(c)
+		c.Set("newPath", newPath)
+		c.Next()
+	}
+}
+
+// Original path: /api/v3/videosearch/events/groups/3/C-L1-28/aggregates/value/1234
+// New path: /api/v3/videosearch/events/groups/*/aggregates/value/*
+
+// Original path: /api/v3/videosearch/events/groups/3 C-L1-28/aggregates/value/12/34
+// New path: /api/v3/videosearch/events/groups/*/aggregates/value/*
